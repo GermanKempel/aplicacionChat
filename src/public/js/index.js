f53d95cdd97c05feb257e59d4c0268e0560f1e4d
GermanKempel <111395596+GermanKempel@users.noreply.github.com>
2023-04-25 19:56:39 -0300

    Swal

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,23 @@
 const socket = io();
+
+// Swal.fire({
+//   title: 'Saludos!',
+//   text: 'Bienvenido al chat!',
+//   icon: 'success',
+// });
+
+let user;
+const chatbox = document.getElementById("chatBox");
+
+Swal.fire({
+  title: 'Bienvenido!',
+  text: 'Ingresa tu nombre de usuario',
+  input: 'text',
+  inputValidator: (value) => {
+    return !value && 'Debes ingresar un nombre de usuario!'
+  },
+  allowOutsideClick: false,
+  allowEscapeKey: false,
+}).then(result => {
+  user = result.value;
+});
