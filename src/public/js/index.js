8dd1d298ffbe12047e9b99e1ac38bca2a68c4a18
GermanKempel <111395596+GermanKempel@users.noreply.github.com>
2023-04-25 20:29:52 -0300

    3ra parte chat

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -20,4 +20,34 @@ Swal.fire({
   allowEscapeKey: false,
 }).then(result => {
   user = result.value;
+  socket.emit("authenticated", user);
+});
+
+chatbox.addEventListener("keyup", evt => {
+  if (evt.key === "Enter") {
+    if (chatbox.value.trim().lenght > 0) {
+      socket.emit("message", { user, message: chatbox.value });
+      chatbox.value = "";
+    }
+  }
+});
+
+socket.on("messageLogs", data => {
+  let log = document.getElementById("messageLogs");
+  let messages = '';
+  data.forEach(message => {
+    messages += `${message.user}dice: ${message.message}<br/>`;
+  });
+  log.innerHTML = messages;
+});
+
+socket.on("newUserConnected", data => {
+  Swal.fire({
+    taost: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    title: `El usuario ${data} se ha conectado al chat!`,
+    icon: 'success',
+  });
 });
